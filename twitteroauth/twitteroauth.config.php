166e6e0c99f2e92b36a05e7a29200a9e546758b4
poseurtech <poseurtech@abrah.am>
2009-03-31 02:51:46 -0400

    added config file

---
+++ twitteroauth/twitteroauth.config.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Twitter OAuth */
+/* Consumer key from twitter */
+$consumer_key = 'CONSUMER_KEY_HERE';
+/* Consumer secret from twitter */
+$consumer_secret = 'CONSUMER_SECRET_HERE';
+/* API base URL */
+$api_base_url = 'https://twitter.com';
+/* Home page */
+$home_page = 'http://twitteroauth.labs.poseurtech.com';
+/* Callback URL */
+$callback_url = $home_page.'/callback.php';
